b099819aa8cf375db04f5b2b7b513c5d1a8f4a5d
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-23 10:55:20 +0000

    Initial commit - Billing Service

---
+++ src/main/java/com/smart/mobility/smartmobilitybillingservice/SmartMobilityBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smart.mobility.smartmobilitybillingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMobilityBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMobilityBillingServiceApplication.class, args);
+    }
+
+}
