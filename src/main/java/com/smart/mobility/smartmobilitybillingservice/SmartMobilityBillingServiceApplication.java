05bbc244817d06244daff780e776398811bb9bbb
Sidi Ahmed Tidjane Sarr <sats0264@gmail.com>
2026-02-24 20:07:28 +0000

    Enable scheduling for periodic tasks in SmartMobilityBillingServiceApplication

--- src/main/java/com/smart/mobility/smartmobilitybillingservice/SmartMobilityBillingServiceApplication.java
+++ src/main/java/com/smart/mobility/smartmobilitybillingservice/SmartMobilityBillingServiceApplication.java
@@ -2,12 +2,16 @@ package com.smart.mobility.smartmobilitybillingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+/*** @EnableScheduling allows us to use @Scheduled annotations for periodic tasks,
+ * such as resetting daily spent amounts at midnight.
+ */
+@EnableScheduling
 public class SmartMobilityBillingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartMobilityBillingServiceApplication.class, args);
     }
-
 }
